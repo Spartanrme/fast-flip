9751771cb35e149b85cb7e83e819f820775ff8de
Josh Rodgers <bytemr@gmail.com>
2021-04-03 14:38:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fast Flip!
+Foundry VTT add-on that allows for fast mirroring of selected tokens using a hotkey (default `F`).
---
+++ main.js
@@ -0,0 +1,29 @@
+const MODULE_NAME = "fast-flip";
+const FLIP_TOKEN_HOT_KEY = `${MODULE_NAME}.flip-token`;
+
+Hooks.once("ready", () => {
+    if (!game.modules.get("lib-df-hotkeys")?.active) {
+        if (game.user.isGM) {
+            ui.notifications.error("'Fast Flip!' requires the 'Library: DF Hotkeys' module. Please install and activate this dependency.");
+        }
+
+        return;
+    }
+
+    Hotkeys.registerShortcut(
+        {
+            name: FLIP_TOKEN_HOT_KEY,
+            label: FLIP_TOKEN_HOT_KEY,
+            default: () => { return { key: Hotkeys.keys.KeyF, alt: false, ctrl: false, shift: false }; },
+            onKeyDown: handleTokenFlip,
+        }
+    );
+});
+
+async function handleTokenFlip() {
+    const controlledTokens = game.canvas.tokens.controlled;
+
+    for (const token of controlledTokens) {
+        await token.update({ "mirrorX": !token.data.mirrorX });
+    }
+}
